a68773ac2effad81c6e07213fa1a3815e5caa84a
Christian Hahn <christianhahn09@gmail.com>
2019-11-09 21:40:46 -0800

    add setup.py & setup.cfg

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='windfreak',
+    version='0.1.0',
+    author='Christian Hahn',
+    author_email='christianhahn09@gmail.com',
+    description='Python package for Windfreak Technologies devices.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/christian-hahn/windfreak-python',
+    packages=find_packages(exclude=['tests', 'examples']),
+    install_requires=[
+        'pyserial',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    license='MIT',
+    python_requires='>=3',
+)
